7c583103a785a9164d826d2c41fffaa616795f11
stickler-ci <support@stickler-ci.com>
2019-06-12 19:35:15 +0000

    Fixing style errors.

--- bin/main.rb
+++ bin/main.rb
@@ -1,104 +1,102 @@
+# frozen_string_literal: true
+
 ## control the game
 
-require File.expand_path("../lib/script.rb")
-#for other users just remove the '#'
-#require File.expand_path("lib/script.rb")
+require File.expand_path('../lib/script.rb')
+# for other users just remove the '#'
+# require File.expand_path("lib/script.rb")
 
-puts "/////////////// Welcome to the tic_tac-toe game /////////////"
+puts '/////////////// Welcome to the tic_tac-toe game /////////////'
 player_1 = nil
-  player_2 = nil
-  loop do
-    puts "Please enter Player_1 name"
-    name = gets.chomp
-    valid = name.scan(/\D/).empty?
-    if valid
-      puts "Invalid name!"
-    else
-      player_1 = Player.new(name)
-      puts "PLAYER 1: #{player_1.name}"
-      break
-    end
-  end
-  puts "Please select you choice 'x' or 'o'"
-  choice = gets.chomp.upcase!
-  loop do
-    if choice == 'X' || choice == 'O'
-      player_1.choice = choice
-      break
-    else
-      puts 'Enter a valid choice'
-      choice = gets.chomp.upcase!
-    end
-  end
-
-
-  loop do
-    puts "Please enter Player_2 name"
-    name = gets.chomp
-    valid = name.scan(/\D/).empty?
-    if valid
-      puts "Invalid name!"
-    else
-      player_2 = Player.new(name)
-      break
-    end
-  end
-  if player_1.choice == 'X'
-      player_2.choice = 'O'
-  else
-      player_2.choice = 'X'
-  end
-  puts "PLAYER 2 CHOICE: #{player_2.choice}"
+player_2 = nil
 loop do
-  
+  puts 'Please enter Player_1 name'
+  name = gets.chomp
+  valid = name.scan(/\D/).empty?
+  if valid
+    puts 'Invalid name!'
+  else
+    player_1 = Player.new(name)
+    puts "PLAYER 1: #{player_1.name}"
+    break
+  end
+end
+puts "Please select you choice 'x' or 'o'"
+choice = gets.chomp.upcase!
+loop do
+  if choice == 'X' || choice == 'O'
+    player_1.choice = choice
+    break
+  else
+    puts 'Enter a valid choice'
+    choice = gets.chomp.upcase!
+  end
+end
 
-  
+loop do
+  puts 'Please enter Player_2 name'
+  name = gets.chomp
+  valid = name.scan(/\D/).empty?
+  if valid
+    puts 'Invalid name!'
+  else
+    player_2 = Player.new(name)
+    break
+  end
+end
+player_2.choice = if player_1.choice == 'X'
+                    'O'
+                  else
+                    'X'
+                  end
+puts "PLAYER 2 CHOICE: #{player_2.choice}"
+loop do
   board = Board.new
   board.display
 
-  puts "*** Start to Play Now ***"
+  puts '*** Start to Play Now ***'
 
   loop do
     begin
-        puts "player_1 :Choose a place between 1 and 9"
-        place = gets.chomp
-        result = board.update(player_1.choice, place.to_i)
-    rescue StandardError=>e
-        puts "Error,You cannot use this place !: #{e}"
-        puts 'Try again :)'
-        retry
+      puts 'player_1 :Choose a place between 1 and 9'
+      place = gets.chomp
+      result = board.update(player_1.choice, place.to_i)
+    rescue StandardError => e
+      puts "Error,You cannot use this place !: #{e}"
+      puts 'Try again :)'
+      retry
     end
     if result[0] == true
-        puts "#{player_1.name} wins"
-        player_1.score+=1
-        puts "#{player_1.name} score is: #{player_1.score}"
-        puts "#{player_2.name} score is: #{player_2.score}"
-        break
+      puts "#{player_1.name} wins"
+      player_1.score += 1
+      puts "#{player_1.name} score is: #{player_1.score}"
+      puts "#{player_2.name} score is: #{player_2.score}"
+      break
     end
     if result[1] == 9
-        puts "*** It is draw ***"
-        puts "#{player_1.name} score is: #{player_1.score}"
-        puts "#{player_2.name} score is: #{player_2.score}"
-        break
+      puts '*** It is draw ***'
+      puts "#{player_1.name} score is: #{player_1.score}"
+      puts "#{player_2.name} score is: #{player_2.score}"
+      break
     end
     begin
-        puts "player_2 :Choose a place between 1 and 9"
-        place = gets.chomp
-        result = board.update(player_2.choice, place.to_i)
-    rescue StandardError=>e
-        puts "Error,You cannot use this place !: #{e}"
-        puts 'Try again :)'
-        retry
+      puts 'player_2 :Choose a place between 1 and 9'
+      place = gets.chomp
+      result = board.update(player_2.choice, place.to_i)
+    rescue StandardError => e
+      puts "Error,You cannot use this place !: #{e}"
+      puts 'Try again :)'
+      retry
     end
-    if result[0] == true
-        puts "#{player_2.name} wins"
-        player_2.score+=1
-        puts "#{player_1.name} score is: #{player_1.score}"
-        puts "#{player_2.name} score is: #{player_2.score}"
-        break
-    end
+    next unless result[0] == true
+
+    puts "#{player_2.name} wins"
+    player_2.score += 1
+    puts "#{player_1.name} score is: #{player_1.score}"
+    puts "#{player_2.name} score is: #{player_2.score}"
+    break
   end
-  
-  puts "Do you want to play again? (y / n)"
+
+  puts 'Do you want to play again? (y / n)'
   break if gets.chomp == 'n'
 end
--- lib/script.rb
+++ lib/script.rb
@@ -1,45 +1,49 @@
+# frozen_string_literal: true
+
 # implementing the player class
 
 class Player
-    attr_accessor :choice
-    attr_reader :name
-    def initialize (name)
-        @choice = nil
-        @name = name
-    end
+  attr_accessor :choice
+  attr_reader :name
+  def initialize(name)
+    @choice = nil
+    @name = name
+  end
 end
 # implementing the Board class
 class Board
-    attr_writer :matrix
-    def initialize ()
-        @counter = 0
-        @matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-    end
-    def update (choice,place)
-        if place <=3
-            @matrix[0][@matrix[0].index(place)] = choice
+  attr_writer :matrix
+  def initialize
+    @counter = 0
+    @matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+  end
 
-        elsif place <=6
-            @matrix[1][@matrix[1].index(place)] = choice
+  def update(choice, place)
+    if place <= 3
+      @matrix[0][@matrix[0].index(place)] = choice
 
-        else
-            @matrix[2][@matrix[2].index(place)] = choice
-        end
-        display
-        result = Result.new(@matrix)
-        result.check_success
-        @counter += 1
-        return[result.check_success, @counter]
+    elsif place <= 6
+      @matrix[1][@matrix[1].index(place)] = choice
+
+    else
+      @matrix[2][@matrix[2].index(place)] = choice
     end
-    def display
-        puts "---------"
-        puts "#{@matrix[0][0]} | #{@matrix[0][1]} | #{@matrix[0][2]}"
-        puts "---------"
-        puts "#{@matrix[1][0]} | #{@matrix[1][1]} | #{@matrix[1][2]}"
-        puts "---------"
-        puts "#{@matrix[2][0]} | #{@matrix[2][1]} | #{@matrix[2][2]}"
-        puts "---------"
-    end
+    display
+    result = Result.new(@matrix)
+    result.check_success
+    @counter += 1
+    [result.check_success, @counter]
+  end
+
+  def display
+    puts '---------'
+    puts "#{@matrix[0][0]} | #{@matrix[0][1]} | #{@matrix[0][2]}"
+    puts '---------'
+    puts "#{@matrix[1][0]} | #{@matrix[1][1]} | #{@matrix[1][2]}"
+    puts '---------'
+    puts "#{@matrix[2][0]} | #{@matrix[2][1]} | #{@matrix[2][2]}"
+    puts '---------'
+  end
 end
 
 class Result
@@ -48,35 +52,42 @@ class Result
   end
 
   public
+
   def check_success
     check_row || check_diagonal || check_column
   end
+
   private
+
   def check_row
-    if (@matrix[0][0] == @matrix[0][1] && @matrix[0][1] == @matrix[0][2] ||
-        @matrix[1][0] == @matrix[1][1] && @matrix[1][1] == @matrix[1][2] ||
-        @matrix[2][0] == @matrix[2][1] && @matrix[2][1] == @matrix[2][2])
-        return true
+    if @matrix[0][0] == @matrix[0][1] && @matrix[0][1] == @matrix[0][2] ||
+       @matrix[1][0] == @matrix[1][1] && @matrix[1][1] == @matrix[1][2] ||
+       @matrix[2][0] == @matrix[2][1] && @matrix[2][1] == @matrix[2][2]
+      return true
     end
+
     false
   end
+
   def check_column
-    if (@matrix[0][0] == @matrix[1][0] && @matrix[1][0] == @matrix[2][0] ||
-        @matrix[0][1] == @matrix[1][1] && @matrix[1][1] == @matrix[2][1] ||
-        @matrix[0][2] == @matrix[1][2] && @matrix[1][2] == @matrix[2][2])
-        return true
+    if @matrix[0][0] == @matrix[1][0] && @matrix[1][0] == @matrix[2][0] ||
+       @matrix[0][1] == @matrix[1][1] && @matrix[1][1] == @matrix[2][1] ||
+       @matrix[0][2] == @matrix[1][2] && @matrix[1][2] == @matrix[2][2]
+      return true
     end
+
     false
   end
+
   def check_diagonal
-    if (@matrix[0][0] == @matrix[1][1] && @matrix[1][1] == @matrix[2][2] ||
-        @matrix[2][0] == @matrix[1][1] && @matrix[1][1] == @matrix[0][2])
-        return true
+    if @matrix[0][0] == @matrix[1][1] && @matrix[1][1] == @matrix[2][2] ||
+       @matrix[2][0] == @matrix[1][1] && @matrix[1][1] == @matrix[0][2]
+      return true
     end
+
     false
   end
 end
 
-
 new_board = Board.new
-new_board.update('x',1)
+new_board.update('x', 1)
