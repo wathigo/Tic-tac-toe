9ea4f0c06c14ce3ac7dea555e5686aefd3b62dbe
wathigo <wathigosimon@gmail.com>
2019-06-12 16:00:04 -0400

    fix linter errors

--- bin/main.rb
+++ bin/main.rb
@@ -7,17 +7,17 @@ require File.expand_path('../lib/script.rb')
 # require File.expand_path("lib/script.rb")
 
 puts '/////////////// Welcome to the tic_tac-toe game /////////////'
-player_1 = nil
-player_2 = nil
+player1 = nil
+player2 = nil
 loop do
-  puts 'Please enter Player_1 name'
+  puts 'Please enter player1 name'
   name = gets.chomp
   valid = name.scan(/\D/).empty?
   if valid
     puts 'Invalid name!'
   else
-    player_1 = Player.new(name)
-    puts "PLAYER 1: #{player_1.name}"
+    player1 = Player.new(name)
+    puts "PLAYER 1: #{player1.name}"
     break
   end
 end
@@ -25,7 +25,7 @@ puts "Please select you choice 'x' or 'o'"
 choice = gets.chomp.upcase!
 loop do
   if choice == 'X' || choice == 'O'
-    player_1.choice = choice
+    player1.choice = choice
     break
   else
     puts 'Enter a valid choice'
@@ -34,22 +34,18 @@ loop do
 end
 
 loop do
-  puts 'Please enter Player_2 name'
+  puts 'Please enter player2 name'
   name = gets.chomp
   valid = name.scan(/\D/).empty?
   if valid
     puts 'Invalid name!'
   else
-    player_2 = Player.new(name)
+    player2 = Player.new(name)
     break
   end
 end
-player_2.choice = if player_1.choice == 'X'
-                    'O'
-                  else
-                    'X'
-                  end
-puts "PLAYER 2 CHOICE: #{player_2.choice}"
+player2.choice = player1.choice == 'X' ? 'O' :  'X'
+puts "PLAYER 2 CHOICE: #{player2.choice}"
 loop do
   board = Board.new
   board.display
@@ -58,42 +54,40 @@ loop do
 
   loop do
     begin
-      puts 'player_1 :Choose a place between 1 and 9'
+      puts 'player1 :Choose a place between 1 and 9'
       place = gets.chomp
-      result = board.update(player_1.choice, place.to_i)
-    rescue StandardError => e
-      puts "Error,You cannot use this place !: #{e}"
-      puts 'Try again :)'
+      result = board.update(player1.choice, place.to_i)
+    rescue
+      puts "Error,You cannot use this place ! \n Try again :)"
       retry
     end
     if result[0] == true
-      puts "#{player_1.name} wins"
-      player_1.score += 1
-      puts "#{player_1.name} score is: #{player_1.score}"
-      puts "#{player_2.name} score is: #{player_2.score}"
+      puts "#{player1.name} wins"
+      player1.score += 1
+      puts "#{player1.name} score is: #{player1.score}"
+      puts "#{player2.name} score is: #{player2.score}"
       break
     end
     if result[1] == 9
       puts '*** It is draw ***'
-      puts "#{player_1.name} score is: #{player_1.score}"
-      puts "#{player_2.name} score is: #{player_2.score}"
+      puts "#{player1.name} score is: #{player1.score}"
+      puts "#{player2.name} score is: #{player2.score}"
       break
     end
     begin
-      puts 'player_2 :Choose a place between 1 and 9'
+      puts 'player2 :Choose a place between 1 and 9'
       place = gets.chomp
-      result = board.update(player_2.choice, place.to_i)
-    rescue StandardError => e
-      puts "Error,You cannot use this place !: #{e}"
-      puts 'Try again :)'
+      result = board.update(player2.choice, place.to_i)
+    rescue
+      puts "Error,You cannot use this place ! \n Try again :)"
       retry
     end
     next unless result[0] == true
 
-    puts "#{player_2.name} wins"
-    player_2.score += 1
-    puts "#{player_1.name} score is: #{player_1.score}"
-    puts "#{player_2.name} score is: #{player_2.score}"
+    puts "#{player2.name} wins"
+    player2.score += 1
+    puts "#{player1.name} score is: #{player1.score}"
+    puts "#{player2.name} score is: #{player2.score}"
     break
   end
 
