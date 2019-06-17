dd3b7e5b7cfc63c27f2823d96967e70e80afdef6
othmane <othman-19@hotmail.fr>
2019-06-17 12:46:41 +0100

    add the validate_choice and validate_name methods

--- bin/main.rb
+++ bin/main.rb
@@ -5,6 +5,7 @@
 require File.expand_path('../lib/script.rb')
 # for other users just remove the '#'
 # require File.expand_path("lib/script.rb")
+
 def display(matrix)
   puts '---------'
   puts "#{matrix[0][0]} | #{matrix[0][1]} | #{matrix[0][2]}"
@@ -14,46 +15,59 @@ def display(matrix)
   puts "#{matrix[2][0]} | #{matrix[2][1]} | #{matrix[2][2]}"
   puts '---------'
 end
-puts '/////////////// Welcome to the tic_tac-toe game /////////////'
-player1 = nil
-player2 = nil
-loop do
-  puts 'Please enter player1 name'
-  name = gets.chomp
-  valid = name.scan(/\D/).empty?
-  if valid
-    puts 'Invalid name!'
-  else
-    player1 = Player.new(name)
-    puts "PLAYER 1: #{player1.name}"
-    break
+
+
+
+def validate_name(name)
+  loop
+    valid = name.scan(/\D/).empty?
+    if valid
+      puts "Enter a valid name!"
+      name = gets.chomp
+    else
+      name
+      break
+    end
   end
 end
-puts "Please select you choice 'x' or 'o'"
-choice = gets.chomp.upcase!
-loop do
-  if choice == 'X' || choice == 'O'
-    player1.choice = choice
+
+
+def validate_choice(choice)
+  loop do
+    if choice == 'X' || choice == 'O'
+    choice
     break
   else
-    puts 'Enter a valid choice'
+    puts 'Enter a valid choice' 
     choice = gets.chomp.upcase!
   end
 end
 
-loop do
-  puts 'Please enter player2 name'
-  name = gets.chomp
-  valid = name.scan(/\D/).empty?
-  if valid
-    puts 'Invalid name!'
-  else
-    player2 = Player.new(name)
-    break
-  end
-end
+puts '/////////////// Welcome to the tic_tac-toe game /////////////'
+
+player1 = nil
+player2 = nil
+
+puts 'Please enter player1 name'
+name = gets.chomp
+player1.name = validate_name(name)
+
+
+puts "Please select you choice 'x' or 'o'"
+choice = gets.chomp.upcase!
+player1.choice = validate_choice(choice)
+
+
+puts 'Please enter player2 name'
+name = gets.chomp
+player2.name = validate_name(name)
+
 player2.choice = player1.choice == 'X' ? 'O' : 'X'
 puts "PLAYER 2 CHOICE: #{player2.choice}"
+
+
+
+
 loop do
   board = Board.new
   matrix = board.matrix
